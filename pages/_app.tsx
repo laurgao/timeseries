7c33aa756e40f2603a3f7a81d9ebe84fe2f398c2
Laura Gao <lauragao86868@gmail.com>
2021-09-29 11:03:21 -0400

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import "../styles/globals.css";
+import {Provider} from "next-auth/client";
+import ReactModal from "react-modal";
+import Navbar from "../components/Navbar";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", (url, {shallow}) => {
+    if (!shallow) NProgress.start();
+});
+Router.events.on("routeChangeComplete", (url, {shallow}) => {
+    if (!shallow) NProgress.done();
+});
+Router.events.on("routeChangeError", () => NProgress.done());
+
+export default function App({Component, pageProps}) {
+    return (
+        <Provider session={pageProps.session}>
+            <Navbar/>
+            <div id="app-root">
+                <Component {...pageProps} />
+            </div>
+        </Provider>
+    );
+}
+
+ReactModal.setAppElement("#app-root");
