dfde14e73f213bea80f940a7907d8f3311ad2785
Laura Gao <lauragao86868@gmail.com>
2021-09-30 13:42:33 -0400

    Notionbutton visual improvement

--- components/headless/NotionButton.tsx
+++ components/headless/NotionButton.tsx
@@ -1,12 +1,12 @@
 import { FaPlus } from "react-icons/fa";
-import Button from "./Button"
+import Button from "./Button";
 
 const NotionButton = ({onClick, children} : {
     onClick: () => any,
     children: string
 }) => {
     return (
-        <Button className="opacity-40 transition cursor-pointer py-4 px-2 hover:bg-gray-100 rounded-xl" onClick={onClick}>
+        <Button containerClassName="opacity-40 transition cursor-pointer py-4 px-3 hover:bg-gray-100 rounded-xl w-full" onClick={onClick}>
             <div className="flex items-center">
                 <FaPlus/>
                 <p className="ml-2.5">{children}</p>
