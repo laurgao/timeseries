a90ffb462e39be825e1ebee1b00f955920d628b1
Laura Gao <lauragao86868@gmail.com>
2022-01-29 11:37:07 -0500

    Make the random theme color same for every day

--- utils/utils.ts
+++ utils/utils.ts
@@ -21,6 +21,19 @@ const tailwindColors = [
     "rose",
 ];
 
-const colorIndex = Math.floor(Math.random() * tailwindColors.length - 1) + 1;
+const sumDigits = (n: number) => n.toString().split("").map(Number).reduce((a, b) => (a + b), 0)
+const mulDigits = (n: number) => n.toString().split("").map(Number).reduce((a, b) => (a * b), 1)
+
+// const colorIndex = Math.floor(Math.random() * tailwindColors.length - 1) + 1;
+const getPseudoRandomNumber = (date: Date) => {
+    var index = 1
+    index *= sumDigits(date.getDate())
+    index *= sumDigits(date.getMonth() + 1)
+    index *= sumDigits(date.getFullYear())
+    return index - 1
+}
+const today = new Date()
+const colorIndex = getPseudoRandomNumber(today) % (tailwindColors.length - 1)
+
 export const color = tailwindColors[colorIndex];
 export default tailwindColors;
